475c16508f0a9135b41f933439cb70cf9cdda76e
J32RY <jerry.maillot@epita.fr>
2014-04-21 02:46:35 +0200

    bonne nuit

--- Arcadia/Arcadia/Space Invaders/ennemi.cs
+++ Arcadia/Arcadia/Space Invaders/ennemi.cs
@@ -16,24 +16,28 @@ namespace Arcadia.Space_Invaders
     class ennemi
     {
         private Texture2D enn;
+
+        public Texture2D enn_public { get { return enn; } }
+       
         bool is_alive;
         
         public virtual void Initialize()
         {
             is_alive = true;
         }
-        public virtual void LoadContent(ContentManager content, string typ_enn) // jai passé le truc en fonction retournant une texture pour le draw 
+        public virtual void LoadContent(ContentManager content, string type_enn)
         {
-            enn = content.Load<Texture2D>(typ_enn);
+            enn = content.Load<Texture2D>(type_enn);
+            
            
         }
 
-        public virtual void Draw(SpriteBatch spriteBatch, int pos_x, int pos_y)
+        public virtual void Draw(SpriteBatch spriteBatch,Texture2D ennemi, int pos_x, int pos_y)
         {
             spriteBatch.Begin();
             spriteBatch.Draw(enn, new Vector2(pos_x,pos_y), Microsoft.Xna.Framework.Color.White);
             spriteBatch.End();
         }
 
-    }
+}
 }
--- Arcadia/Arcadia/Space Invaders/ennemi_game.cs
+++ Arcadia/Arcadia/Space Invaders/ennemi_game.cs
@@ -21,6 +21,7 @@ namespace Arcadia.Space_Invaders
         public virtual void Initialize(ContentManager content) // jai passé un contentmanager en parametre sinon ca passe pas ds le ennemi_tableau[x, ...].LoadContent
         {
             ennemi_tableau = new ennemi[10, 4];
+            
             for (int x = 0; x < 10; x++)
             {
                 ennemi_tableau[x, 1] = new ennemi();
@@ -34,21 +35,46 @@ namespace Arcadia.Space_Invaders
                 ennemi_tableau[x, 3].LoadContent(content, "SpaceInvaders/ennemi3");
                 ennemi_tableau[x, 4] = new ennemi();
                 ennemi_tableau[x, 4].Initialize();
-                ennemi_tableau[x, 4].LoadContent(content, "SpaceInvaders/ennemi4");
+                ennemi_tableau[x, 4].LoadContent(content, "SpaceInvaders/ennemi1");
 
             }
-
+            
 
         }
         public virtual void Draw(SpriteBatch spritebatch, GameTime gametime, int pos_x, int pos_y)
         {
             for (int x = 0; x < 10; x++)
             {
-                ennemi_tableau[x, 1].Draw(spritebatch, pos_x, pos_y);
+                ennemi_tableau[x, 1].Draw(spritebatch,ennemi_tableau[x,1].enn_public, pos_x, pos_y);
                 pos_x = pos_x + 5;
-                pos_y = pos_y + 5;
                 
                 
+            }
+            pos_y = pos_y +5;
+
+            for (int x = 0; x < 10; x++)
+            {
+                ennemi_tableau[x, 2].Draw(spritebatch, ennemi_tableau[x, 2].enn_public, pos_x, pos_y);
+                pos_x = pos_x + 5;
+
+            }
+
+            pos_y = pos_y + 5;
+
+            for (int x = 0; x < 10; x++)
+            {
+                ennemi_tableau[x, 3].Draw(spritebatch, ennemi_tableau[x, 3].enn_public, pos_x, pos_y);
+                pos_x = pos_x + 5;
+
+            }
+
+            pos_y = pos_y + 5;
+
+            for (int x = 0; x < 10; x++)
+            {
+                ennemi_tableau[x, 4].Draw(spritebatch, ennemi_tableau[x, 1].enn_public , pos_x, pos_y);
+                pos_x = pos_x + 5;
+
             }
         }
 
